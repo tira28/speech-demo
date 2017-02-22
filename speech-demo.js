93db7ed859d6c3db73a474ce5b903b5aa3b598c8
Yudistira Wachyar <YWachyar@nl.ibm.com>
2017-02-22 14:43:21 +0100

    initial files

---
+++ speech-demo.js
@@ -0,0 +1,64 @@
+/**
+ * Created by yudistira on 2/22/17.
+ */
+
+// Imports the Google Cloud client library
+const Speech = require('@google-cloud/speech');
+
+// Your Google Cloud Platform project ID
+const projectId = 'tira-speech-abn';
+
+// Instantiates a client
+const speechClient = Speech({
+    projectId: projectId
+});
+
+// The name of the audio file to transcribe
+const fileName = './resources/audio.raw';
+
+
+
+/* Synchronous
+
+// The audio file's encoding and sample rate
+const options = {
+    encoding: 'LINEAR16',
+    sampleRate: 16000
+};
+
+// Detects speech in the audio file synchronous
+speechClient.recognize(fileName, options)
+    .then((results) => {
+        const transcription = results[0];
+        console.log(`Transcription: ${transcription}`);
+    });
+
+*/
+
+
+// Asynchronous
+
+let encoding = 'LINEAR16';
+let sampleRate = 16000;
+
+const request = {
+    encoding: encoding,
+    sampleRate: sampleRate
+};
+
+let speech = Speech();
+
+speech.startRecognition(fileName,request)
+    .then(getResults)
+    .then(getTranscription);
+
+function getResults(results){
+    let operation = results[0];
+    return operation.promise();
+};
+
+function getTranscription(transcription){
+    console.log(`Transcription: $(transcription)`);
+};
+
+
